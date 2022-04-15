840517134a957ba0adf98ad67948ea583d168e28
Victor Zhu <ghoyelo@gmail.com>
2022-04-15 11:38:25 +0800

    Widget layout polish

--- ShippedShield/Classes/SSWidgetView.m
+++ ShippedShield/Classes/SSWidgetView.m
@@ -107,7 +107,7 @@
     [_containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[descLabel]|" options:0 metrics:metrics views:views]];
 
     [_titleLabel.topAnchor constraintEqualToAnchor:_containerView.topAnchor constant:-2.5].active = YES;
-    [_descLabel.bottomAnchor constraintEqualToAnchor:_containerView.bottomAnchor constant:0.5].active = YES;
+    [_descLabel.bottomAnchor constraintEqualToAnchor:_containerView.bottomAnchor constant:3].active = YES;
     [_descLabel.rightAnchor constraintEqualToAnchor:_feeLabel.rightAnchor].active = YES;
 }
 
